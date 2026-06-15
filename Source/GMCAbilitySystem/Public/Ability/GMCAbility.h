1cadb6feafe4bff631201b15febe9c26f3dc70dd
Emerson <emerson@tetrastudios.com.au>
2026-06-15 11:15:32 +1000

    added ability tags too

--- Source/GMCAbilitySystem/Public/Ability/GMCAbility.h
+++ Source/GMCAbilitySystem/Public/Ability/GMCAbility.h
@@ -44,6 +44,10 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "GMCAbilitySystem")
 	FGMCAbilityData AbilityData;
 
+	UPROPERTY(BlueprintReadOnly)
+	UGMCAbilityEffect* AbilityCostInstance = nullptr;
+
+
 	// Assign a new, incrementing, Task ID
 	UFUNCTION()
 	int GetNextTaskID(){TaskIDCounter += 1;
@@ -313,7 +317,7 @@ private:
 	UPROPERTY()
 	TArray<TObjectPtr<UGameplayTask>> ActiveTasks;
 
-	UPROPERTY()
+	UPROPERTY(BlueprintReadOnly)
 	UGMCAbilityEffect* AbilityCostInstance = nullptr;
 
 	bool IsOnCooldown() const;
